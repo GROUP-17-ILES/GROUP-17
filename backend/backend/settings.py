18d5099b27b7708b76643b132574bb20392ddf6c
Albert <aherezaalbert680@gmail.com>
2026-05-05 16:49:27 +0300

    l have replaced the default database content to the postgresql structure

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,10 +83,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'), # go to .env and get the value of BD_USER and use it here
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
