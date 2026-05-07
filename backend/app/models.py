47f1174648b2f82990a52d7de97dbb249c494742
jessielillians-crypto <jessielillians@gmail.com>
2026-05-07 19:16:05 +0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES=(
-         ('studentintern','StudentIntern'),
-         ('workplacesupervisor','WorkplaceSupervisor'),
-         ('academicsupervisor','AcademicSupervisor'),
-         ('internshipadministrator','InternshipAdministrator')
+           ('student', 'Student'),
+           ('supervisor', 'Supervisor'),
+           ('admin', 'Admin'),
+         
     )
     name=models.CharField(max_length=30,null=False,blank=False)
     role=models.CharField(max_length=50,choices=ROLE_CHOICES)
@@ -16,15 +14,7 @@ class CustomUser(AbstractUser):
     email=models.EmailField(unique=True)
     ID_number = models.CharField(max_length=20, unique=True) 
     REQUIRED_FIELDS = ['email', 'ID_number'] 
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    class Student(models.Model):
-    users = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    course_title = models.CharField(max_length=50)
-    university_name = models.CharField(max_length=60)
-    year_of_study = models.IntegerField()
-    assigned_supervisor = models.ForeignKey(
-     groups = models.ManyToManyField(
+    groups = models.ManyToManyField(
         'auth.Group',
         related_name='customuser_set',
         blank=True
@@ -36,7 +26,23 @@ class CustomUser(AbstractUser):
     )
     def __str__(self):
         return f"{self.username} ({self.role})"
-
+    
+    
+class Student(models.Model):
+    users = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    course_title = models.CharField(max_length=50)
+    university_name = models.CharField(max_length=60)
+    year_of_study = models.IntegerField()
+    assigned_supervisor = models.ForeignKey(
+     'Supervisor',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='assigned_students'
+    )
+    
+    def __str__(self):
+        return f"{self.users.username} -STUDENT"
     
 class Supervisor(models.Model):
     users = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
@@ -51,6 +57,7 @@ class Admin(models.Model):
 
     def __str__(self):
         return f"{self.users.username} -ADMIN"
+        
 class InternshipPlacement(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     place_of_internship = models.CharField(max_length=100)
@@ -61,6 +68,7 @@ class InternshipPlacement(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.place_of_internship}"
+        
 class WeeklyLog(models.Model):
     STATUS_CHOICES = [
         ('draft', 'Draft'),
@@ -88,83 +96,6 @@ class WeeklyLog(models.Model):
     def __str__(self):
         return f"Week {self.week_number} - {self.user.username} - {self.status}"
     
-
-    
-class AcademicSupervisor(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='academicsupervisor')
-    department = models.CharField(max_length=20, blank=False)
-    employee_id = models.CharField(max_length=20, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.department}"
-    
-
-class StudentIntern(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='studentintern')
-    student_number = models.IntegerField(unique=True)
-    course_of_study = models.CharField(max_length=30, blank=False)
-    university_name = models.CharField(max_length=50, blank=False)
-    current_year_of_study = models.IntegerField(default=1)
-
- 'Supervisor',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='assigned_students'
-
-    def __str__(self):
-        return f"{self.user.username} - {self.student_number}"
-
-class WorkplaceSupervisor(models.Model):
-     user=models.OneToOneField(CustomUser,on_delete=models.CASCADE,related_name='workplacesupervisor')
-     company_name=models.CharField(max_length=40)
-     department=models.CharField(max_length=20)
-     employee_id=models.CharField(max_length=20,unique=True)
-     created_at=models.DateTimeField(auto_now_add=True)
-     updated_at=models.DateTimeField(auto_now=True)
-     def __str__(self):
-         return f'{self.user.username} - {self.company_name}'
-
-class InternshipPlacement(models.Model):
-     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-     company_name=models.CharField(max_length=40)
-     department=models.CharField(max_length=20)
-     supervisior_name=models.CharField(max_length=20)
-     start_date=models.DateField()
-     end_date=models.DateField()
-     def __str__(self):
-         return f'{self.user.username} - {self.company_name}'
-
-class WeeklyLog(models.Model):
-     STATUS_CHOICES=[
-         ('DRAFT' ,'Draft'),
-         ('SUBMITTED','Submitted'),
-         ('APPROVED','Approved'),
-         ('REJECTED','Rejected'),
-     ]
-     user=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-     week_number=models.IntegerField()
-     description=models.TextField()
-     date_submitted=models.DateTimeField(auto_now_add=True)
-     status=models.CharField(
-         max_length=10,
-           choices=STATUS_CHOICES, 
-           default='DRAFT'
-     )
-     def can_transition(self,new_status):
-         allowed_transition={
-             'DRAFT':['SUBMITTED'],
-             'SUBMITTED':['APPROVED','REJECTED'],
-             'REJECTED':['DRAFT'],
-             'APPROVED':[],
-
-         }
-         return new_status in allowed_transition.get(self.status, [])
-     def __str__(self):
-         return f'Week{self.week_number}  -  {self.user.username} '
-
 class EvaluationCriteria(models.Model):
     CRITERIA_CHOICES =[
         ('technical', 'Technical Skills'),
@@ -189,7 +120,8 @@ class Evaluation(models.Model):
 
     def __str__(self):
         return f"{self.placement.user.username} - {self.criteria}: {self.score}"
-        class Feedback(models.Model):
+        
+class Feedback(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='feedback')
     subject = models.CharField(max_length=150)
     message = models.TextField()
@@ -198,8 +130,7 @@ class Evaluation(models.Model):
 
     def __str__(self):
         return f"{self.subject} - {self.user.username}"
-
-
+        
 class SiteSetting(models.Model):
     site_name = models.CharField(max_length=100, default='ILES')
     admin_email = models.EmailField(blank=True)
@@ -215,10 +146,13 @@ class Notification(models.Model):
     message = models.TextField()
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created_at']
 
-
-
-class StatusHistory(models.Model):
+    def __str__(self):
+        return f"{self.title} - {self.user.username}
+        
+ class StatusHistory(models.Model):
     log=models.ForeignKey(WeeklyLog,on_delete=models.CASCADE,related_name='history')
     old_status=models.CharField(max_length=10)
     new_status=models.CharField(max_length=10)
@@ -226,39 +160,9 @@ class StatusHistory(models.Model):
     changed_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
          return f"{self.old_status} - {self.new_status}"
-class EvaluationCriteria(models.Model):
-     name=models.CharField(max_length=20)
-     max_score=models.IntegerField()
-     def __str__(self):
-         return self.name
-
-class Evaluation(models.Model):
-     user=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-     criteria=models.ForeignKey(EvaluationCriteria,on_delete=models.CASCADE)
-     score=models.IntegerField()
-     comments=models.TextField(blank=True,null=True)
-     skills_learned=models.TextField(blank=True,null=True)
-     challenges_faced=models.TextField(blank=True,null=True)
-     feedback=models.TextField(blank=True,null=True)
-     week_number=models.IntegerField(default=1)
-     
-     def __str__(self):
-         return f'{self.user.username}  -  {self.criteria.name}'
-         class Evaluation(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    placement = models.ForeignKey(InternshipPlacement, on_delete=models.CASCADE)
-    criteria = models.ForeignKey(EvaluationCriteria, on_delete=models.SET_NULL, null=True)
-    score = models.PositiveIntegerField()
-    comment = models.TextField(blank=True)
-    evaluation_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.placement.user.username} - {self.criteria}: {self.score}"
-
-
-
 
 class StudentGrade(models.Model):
+     criteria=models.ForeignKey(EvaluationCriteria, on_delete=models.CASCADE)
      efficiency=models.IntegerField
      time_management=models.IntegerField
      problem_solving=models.IntegerField
@@ -266,11 +170,11 @@ class StudentGrade(models.Model):
 
      def save(self, *args, **kwargs):
          self.total_score=(
-            (self.efficiency*0.25) +
-            (self.time_management*0.250) +
-            (self.problemsolving*0.25) +
-            (self.professionalism*0.25)
-         )
+            (self.efficiency*0.25) + #type: ignore
+            (self.time_management*0.250) + #type: ignore
+            (self.problemsolving*0.25) + #type: ignore
+            (self.professionalism*0.25) #type: ignore
+             ) 
 
          if self.total_score>=80:
              self.grade='A'
@@ -284,10 +188,7 @@ class StudentGrade(models.Model):
              self.grade='F'
          super().save(*args, **kwargs)
 
-class Meta:
-     unique_together =['student']
 
-     def __str__(self):
-         return f'{self.user.username}  -  {self.criteria.name}'
-from django.db import models
+
+
 
