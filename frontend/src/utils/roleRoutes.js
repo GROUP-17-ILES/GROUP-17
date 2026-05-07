0ddf7928c4e39265e9dce90375deb18885c98afc
Albert <aherezaalbert680@gmail.com>
2026-05-08 00:23:11 +0300

    Delete frontend directory bse it has been added by accident

--- frontend/src/utils/roleRoutes.js
+++
@@ -1,9 +0,0 @@
-export const ROLE_ROUTES = {
-  admin: "/admin",
-  supervisor: "/supervisor",
-  student: "/student",
-};
-
-export function getRoleRoute(role) {
-  return ROLE_ROUTES[role] || "/login";
-}
