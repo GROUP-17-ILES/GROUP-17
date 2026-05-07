7f91bf10ce626a4a26fd5f1fb965d949654cafda
jessielillians-crypto <jessielillians@gmail.com>
2026-05-07 19:39:42 +0300

    Refactor URL configuration for backend project
    Updated urlpatterns to include app URLs.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,16 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from app.views import *
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/interns/', StudentInternView.as_view(), name='studentintern-list'),
-    path('', AcademicSupervisorView.as_view(), name='academic-supervisor-list'),
-    path('api/workplace-supervisors/', WorkplaceSupervisorView.as_view(), name='workplace-supervisor-list'),
-    path('api/internship-placements/', InternshipPlacementView.as_view(), name='internship-placement-list'),
-    path('api/user-profiles/', UserProfileView.as_view(), name='user-profile-list'),
-    path('api/user-details/', UserDetailView.as_view(), name='user-detail-list'),
-    path('api/evaluations/', EvaluationView.as_view(), name='evaluation-list'),
-    path('api/evaluation-criteria/', EvaluationCriteriaView.as_view(), name='evaluation-criteria-list'),
-    path('api/weekly-logs/', WeeklyLogView.as_view(), name='weekly-log-list'),
-    
+    path('api/', include('app.urls')),
 ]
