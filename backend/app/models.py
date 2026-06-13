7bd93018650d66752cee9fefc3fb3c51d145cb62
jessielillians-crypto <jessielillians@gmail.com>
2026-06-13 20:41:40 +0300

    Refactor models for better structure and clarity

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CustomUser(AbstractUser):
     ROLE_CHOICES=[
            ('student', 'Student'),
