653f26652de1b76755209f1afab57487ed07221c
Albert <aherezaalbert680@gmail.com>
2026-06-13 21:42:11 +0300

    Refactor models in app to improve structure
    Removed the unnecessary space

--- backend/app/models.py
+++ backend/app/models.py
@@ -18,7 +18,7 @@ class CustomUser(AbstractUser):
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='customuser_set',
-        blank=True
+        blank=True  
     )
     user_permissions = models.ManyToManyField(
         'auth.Permission',
